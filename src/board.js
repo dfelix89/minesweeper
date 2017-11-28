f5f4211055395fb3204c3a1d9a39e26fbb088f72
Derek <derekf542@hotmail.com>
2017-11-28 16:09:30 -0700

    Added final steps of game

--- src/board.js
+++ src/board.js
@@ -1,25 +1,4 @@
-class Game {
-    constructor(numberOfRows, numberOfColumns, numberOfBombs) {
-        this._board = new Board(numberOfRows, numberOfColumns, numberOfBombs);
-    }
-    
-    playMove(rowIndex, columnIndex) {
-        this._board.flipTile(rowIndex, columnIndex);
-        if (this._board.playerBoard[rowIndex][columnIndex] === 'B') {
-            console.log('Game over');
-            this._board.print();
-        } else if (this._board.hasSafeTiles()) {
-            console.log('Congrats, you have won!');
-            this._board.print();
-        } else {
-            console.log('Current Board: ');
-            this._board.print();
-        }
-    }
-}
-
-
-class Board {
+export class Board {
     constructor(numberOfRows, numberOfColumns, numberOfBombs) {
         this._numberOfBombs = numberOfBombs;
         this._numberOfTiles = numberOfRows * numberOfColumns;
@@ -109,9 +88,3 @@ static generateBombBoard(numberOfRows, numberOfColumns, numberOfBombs) {
   return board;
 }
 }
-
-
-const g = new Game(3, 3, 3);
-g.playMove(0, 0);
-g.playMove(1, 0);
-g.playMove(2, 0);
